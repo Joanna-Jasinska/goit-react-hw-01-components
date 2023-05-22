b0ee4a7f2c9f8cdc8c355f1618964682d4d59fc5
Annaerith <111195576+Joanna-Jasinska@users.noreply.github.com>
2023-05-22 16:17:09 +0200

    moved app styling to index.css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,17 +9,7 @@ import transactions from './../data/transactions.json';
 
 export const App = () => {
   return (
-    <div
-      style={{
-        flexWrap: 'wrap',
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
+    <div className="app">
       <Profile user={user} />
       <Statistics data={data} />
       <FriendList friends={friends} />
