aea241e00c3c15b24671b8575bb586ea8b684591
Annaerith <111195576+Joanna-Jasinska@users.noreply.github.com>
2023-05-22 14:55:19 +0200

    used prettier
    used prettier, css class isOnline-true/false changed to online/offline

--- src/components/FriendList/friendList.jsx
+++ src/components/FriendList/friendList.jsx
@@ -1,25 +1,27 @@
-import css from "./FriendList.module.css"
-import PropTypes from "prop-types";
+import css from './FriendList.module.css';
+import PropTypes from 'prop-types';
 
-
-export const FriendList =({friends})=>(
-  <ul className={css["friend-list"]}>
-
-  {friends.map(friend=>(
-    <li className={css.item}>
-    <span className={css[`isOnline-${friend.isOnline}`]
-  }></span>
-    <img className={css.avatar} src={friend.avatar} alt="User avatar" width="48" />
-    <p className={css.name}>{friend.name}</p>
-  </li>
-  ))}
-
-  </ul>);
+export const FriendList = ({ friends }) => (
+  <ul className={css['friend-list']}>
+    {friends.map(friend => (
+      <li className={css.item}>
+        <span className={friend.isOnline ? css.online : css.offline}></span>
+        <img
+          className={css.avatar}
+          src={friend.avatar}
+          alt="User avatar"
+          width="48"
+        />
+        <p className={css.name}>{friend.name}</p>
+      </li>
+    ))}
+  </ul>
+);
 
 FriendList.propTypes = {
   friends: PropTypes.shape({
     name: PropTypes.string.isRequired,
     avatar: PropTypes.string.isRequired,
     isOnline: PropTypes.bool.isRequired,
-  })
-}
+  }),
+};
