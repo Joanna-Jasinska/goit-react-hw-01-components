4eba4fed08d2173ef26f0dc74971f5a486412abc
Annaerith <111195576+Joanna-Jasinska@users.noreply.github.com>
2023-05-22 18:37:47 +0200

    prettier?

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,27 +1,25 @@
-import css from "./Statistics.module.css"
-import PropTypes from "prop-types";
+import css from './Statistics.module.css';
+import PropTypes from 'prop-types';
 
+export const Statistics = ({ data }) => (
+  <section className={css.statistics}>
+    <h2 className={css.title}>Upload stats</h2>
 
-export const Statistics =({data})=>(
-<section className={css.statistics}>
-  <h2 className={css.title}>Upload stats</h2>
-
-  <ul className={css["stat-list"]}>
-
-  {data.map(stat=>(
-    <li className={css.item} id={css[stat.id]}>
-    <span className={css.label}>{stat.label}</span>
-    <span className={css.percentage}>{stat.percentage}%</span>
-  </li>
-  ))}
-
-  </ul>
-</section>);
+    <ul className={css['stat-list']}>
+      {data.map(stat => (
+        <li className={css.item} id={css[stat.id]}>
+          <span className={css.label}>{stat.label}</span>
+          <span className={css.percentage}>{stat.percentage}%</span>
+        </li>
+      ))}
+    </ul>
+  </section>
+);
 
 Statistics.propTypes = {
   data: PropTypes.shape({
     id: PropTypes.string.isRequired,
     label: PropTypes.string.isRequired,
     percentage: PropTypes.number.isRequired,
-  })
-}
+  }),
+};
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,36 +1,36 @@
 import _ from 'lodash';
-import css from "./TransactionHistory.module.css"
-import PropTypes from "prop-types";
+import css from './TransactionHistory.module.css';
+import PropTypes from 'prop-types';
 
+export const TransactionHistory = ({ transactions }) => (
+  <table className={css['transaction-history']}>
+    <thead>
+      <tr>
+        <th>Type</th>
+        <th>Amount</th>
+        <th>Currency</th>
+      </tr>
+    </thead>
 
-export const TransactionHistory =({transactions})=>(
-    <table className={css['transaction-history']}>
-  <thead>
-    <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
-    </tr>
-  </thead>
-
-  <tbody>
-    {transactions.map(transaction=>
-    (<tr>
-      <td> {_.capitalize(transaction.type)}</td>
-      <td>125</td>
-      <td>USD</td>
-    </tr>))
-    }
-  </tbody>
-</table>
+    <tbody>
+      { 
+      transactions.map(transaction => (
+        <tr>
+          <td> {_.capitalize(transaction.type)}</td>
+          <td>125</td>
+          <td>USD</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
 );
 
-TransactionHistory.propTypes = {data: PropTypes.array.isRequired};
+TransactionHistory.propTypes = { data: PropTypes.array.isRequired };
 
 TransactionHistory.propTypes = {
   transactions: PropTypes.shape({
     type: PropTypes.string.isRequired,
     amount: PropTypes.string.isRequired,
     currency: PropTypes.string.isRequired,
-  })
-}
+  }),
+};
